380838c38f00cf6080e53f1394b8c1aabcfbf128
Julian Somoza <julian@animus.com.ar>
2020-11-07 01:47:21 -0300

    Dashboard

--- src/Views/admin/partials/topnav.blade.php
+++ src/Views/admin/partials/topnav.blade.php
@@ -11,7 +11,7 @@
   </div>
 </form>
 <ul class="navbar-nav navbar-right">
-  <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg{{ Auth::user()->unreadNotifications->count() ? ' beep' : '' }}"><i class="far fa-bell"></i></a>
+  <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg"><i class="far fa-bell"></i></a>
     <div class="dropdown-menu dropdown-list dropdown-menu-right">
       <div class="dropdown-header">Notifications
         <div class="float-right">
@@ -19,21 +19,7 @@
         </div>
       </div>
       <div class="dropdown-list-content dropdown-list-icons">
-        @if(Auth::user()->unreadNotifications->count())
-        @for($i = 1; $i < 40; $i++)
-        <a href="#" class="dropdown-item dropdown-item-unread">
-          <div class="dropdown-item-icon bg-primary text-white">
-            <i class="fas fa-code"></i>
-          </div>
-          <div class="dropdown-item-desc">
-            Template update is available now!
-            <div class="time text-primary">2 Min Ago</div>
-          </div>
-        </a>
-        @endfor
-        @else
-        <p class="text-muted p-2 text-center">No notifications found!</p>
-        @endif
+        
     </div>
   </li>
   <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
