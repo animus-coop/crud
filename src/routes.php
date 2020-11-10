bb1f53d9c2d69a69e42acf4e101faf6747ed53fb
Julian Somoza <julian@animus.com.ar>
2020-11-09 21:12:35 -0300

    Auth

--- src/routes.php
+++ src/routes.php
@@ -2,12 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::mixin(new \Laravel\Ui\AuthRouteMethods());
 Route::auth(['verify' => true]);
 
 Route::get('admin', function () {
     return view('crud::admin.dashboard.index');
-})->name('admin.dashboard');
+})->name('admin.dashboard')->middleware('auth');
 
 Route::name('admin.')->prefix('admin')->middleware('auth')->group(function() {
     Route::get('users/roles', 'UserController@roles')->name('users.roles');
@@ -18,18 +17,6 @@ Route::name('admin.')->prefix('admin')->middleware('auth')->group(function() {
     ]);
 });
 
-//Route::middleware('auth')->get('logout', function() {
-//    Auth::logout();
-//    return redirect(route('login'))->withInfo('You have successfully logged out!');
-//})->name('logout');
-
-Auth::routes(['verify' => true]);
-
 Route::name('js.')->group(function() {
     Route::get('dynamic.js', 'JsController@dynamic')->name('dynamic');
-});
-
-// Get authenticated user
-Route::get('users/auth', function() {
-    return response()->json(['user' => Auth::check() ? Auth::user() : false]);
 });
