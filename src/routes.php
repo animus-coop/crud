48cc1d1b572a3db2c1591eaec80ed2974e03f8f3
Julian Somoza <julian@animus.com.ar>
2020-11-10 19:04:56 -0300

    Fix crud:: on extended layouts

--- src/routes.php
+++ src/routes.php
@@ -8,17 +8,7 @@ Route::get('/', function () {
 
 
 Route::name('admin.')->middleware('auth')->group(function() {
-    Route::get('users/roles', 'UserController@roles')->name('users.roles');
-    Route::resource('users', 'UserController', [
-        'names' => [
-            'index' => 'users'
-        ]
-    ]);
     Route::get('/dashboard', function () {
         return view('crud::admin.dashboard.index');
     })->name('dashboard');
-});
-
-Route::name('js.')->group(function() {
-    Route::get('dynamic.js', 'JsController@dynamic')->name('dynamic');
 });
