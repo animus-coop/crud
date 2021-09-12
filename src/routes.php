3ca96c9641eb5f727452759fa424cfff28bf5090
Julian Somoza <julian@animus.com.ar>
2021-09-12 19:43:36 -0300

    Adding routes examples

--- src/routes.php
+++ src/routes.php
@@ -2,13 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Dashboard
 Route::get('/', function () {
     return redirect(route('admin.dashboard'));
-})->middleware('auth');
+});
 
+Route::middleware(['auth:sanctum', 'verified', 'role:admin|professional'])->get('/dashboard', 'App\Http\Controllers\admin\DashboardController@index')
+     ->name('admin.dashboard');
 
-Route::name('admin.')->middleware('auth')->group(function() {
-    Route::get('/dashboard', function () {
-        return view('crud::admin.dashboard.index');
-    })->name('dashboard');
+// Users - Admin Auth
+Route::group([
+    'prefix' => 'users',
+    'middleware' => ['auth', 'role:admin']
+], function () {
+    Route::get('/', 'App\Http\Controllers\admin\UserController@index')
+         ->name('user.index');
+    Route::get('/create', 'App\Http\Controllers\admin\UserController@create')
+         ->name('user.create');
+    Route::get('/show/{user}', 'App\Http\Controllers\admin\UserController@show')
+         ->name('user.show')->where('id', '[0-9]+');
+    Route::get('/{user}/edit', 'App\Http\Controllers\admin\UserController@edit')
+         ->name('user.edit')->where('id', '[0-9]+');
+    Route::post('/', 'App\Http\Controllers\admin\UserController@store')
+         ->name('user.store');
+    Route::put('user/{user}', 'App\Http\Controllers\admin\UserController@update')
+         ->name('user.update')->where('id', '[0-9]+');
+    Route::delete('/user/{user}', 'App\Http\Controllers\admin\UserController@destroy')
+         ->name('user.destroy')->where('id', '[0-9]+');
 });
